b376042a69d2d731a03ee5fd0868def7ff270228
Goken5 <fdavipessanha@gmail.com>
2025-07-02 21:20:01 -0300

    create quadradomagico.py

---
+++ py/quadradomagico.py
@@ -0,0 +1,38 @@
+def main():
+    a = int(input())
+    matriz = []
+    for i in range(a):
+        linha = list(map(int, input().split()))
+        matriz.append(linha)
+    
+    ref = sum(matriz[0])
+
+    for i in range(a):
+        if sum(matriz[i]) != ref:
+            print(0)
+            return
+        
+    for i in range(a):
+        soma = 0
+        for j in range(a):
+            soma += matriz[j][i]
+        if soma != ref:
+            print(0)
+            return
+    
+    soma = 0
+    for i in range(a):
+        soma += matriz[i][i]
+    if soma != ref:
+        print(0)
+        return
+    
+    soma = 0
+    for i in range(a):
+        soma += matriz[i][a - i - 1]
+    if soma != ref:
+        print(0)
+        return
+    print(ref)
+main()
+    
